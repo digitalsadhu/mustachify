4550054e1c458a995e494b02286ab6eb94b6c655
Richard Walker <richie@mediasuite.co.nz>
2013-05-25 16:38:16 +0200

    adds angular code to post to server and FF code to trigger image activity

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('mustachifyApp')
-  .controller('MainCtrl', function ($scope) {
-    // $scope.awesomeThings = [
-    //   'HTML5 Boilerplate',
-    //   'AngularJS',
-    //   'Karma'
-    // ];
-  });
+    .controller('headerCtrl', function ($scope) {
+        $scope.pickImage = function () {
+            var a = new Activity({ name: "pick", data: { type: "image/png", multiple: false }});
+            a.onsuccess = function() { 
+                var image = a.result; 
+                var url = "http://mustachify.herokuapp.com";
+                $http.post(url, image).done(function () {
+                    $scope.$apply();
+                });
+            });
+            a.onerror = function() { alert("Failure when trying to pick an image!"); });
+            
+        }
+    })
+    .controller('imageCtrl', function ($scope) {
+        $scope.mustachedImage = "http://mustachify.me/?src=http%3A%2F%2Fmustachify.herokuapp.com%2F";
+    })
+    .controller('shareCtrl', function ($scope) {
+        $scope.setBackground = function () {
+            console.log('set background');
+        }
+    });
